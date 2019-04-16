75338e0cd76ff48aba980c0887bce081f7def970
SOBP <42330344+SOBP@users.noreply.github.com>
2019-04-16 21:52:48 +0900

    fc mistake

--- first.sh
+++ first.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-                cd device/samsung/zerofltexx && nano BoardConfig.mk && nano aosp_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/aosp_zeroltexx.mk && nano aosp_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/aosp_zeroltecan.mk && nano aosp_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/aosp_zerofltecan.mk && nano aosp_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
+                cd device/samsung/zerofltexx && nano BoardConfig.mk && nano aosp_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/aosp_zerofltecan.mk && nano ../zerofltecan.mkaosp_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
                 nano AndroidProducts.mk
                 nano ../zeroltexx/AndroidProducts.sh
                 nano ../zeroltecan/AndroidProducts.sh
@@ -8,5 +8,4 @@
                 nano ../zeroltexx/vendorsetup.sh
                 nano ../zeroltecan/vendorsetup.sh
                 nano ../zerofltecan/vendorsetup.sh
-    esac
 done
