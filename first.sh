ecd5b98c18ed0abbf7a9eb6b257cd26b569415b7
SOBP <42330344+SOBP@users.noreply.github.com>
2019-04-16 21:59:13 +0900

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-                cd device/samsung/zerofltexx && nano BoardConfig.mk && nano aosp_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/aosp_zerofltecan.mk && nano ../zerofltecan.mkaosp_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
+                cd device/samsung/zerofltexx && nano BoardConfig.mk && nano aosp_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/aosp_zerofltecan.mk && nano ../zerofltecan.mk/aosp_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
                 nano AndroidProducts.mk
-                nano ../zeroltexx/AndroidProducts.sh
-                nano ../zeroltecan/AndroidProducts.sh
-                nano ../zerofltecan/AndroidProducts.sh
+                nano ../zeroltexx/AndroidProducts.mk
+                nano ../zeroltecan/AndroidProducts.mk
+                nano ../zerofltecan/AndroidProducts.mk
                 nano vendorsetup.sh
                 nano ../zeroltexx/vendorsetup.sh
                 nano ../zeroltecan/vendorsetup.sh
