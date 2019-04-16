4eac5954ed7e258ffcfd8fd783f3cbc3e67bca4c
SOBP <42330344+SOBP@users.noreply.github.com>
2019-04-16 22:09:37 +0900

    Update and rename Apache2.sh to havoc.sh

---
+++ havoc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+                cd device/samsung/zerofltexx && nano BoardConfig.mk && nano havoc_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/havoc_zeroltexx.mk && nano ../zeroltexx/havoc_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/havoc_zeroltecan.mk && nano ../zeroltecan/havoc_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/havoc_zerofltecan.mk && nano ../zerofltecan.mk/havoc_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
+                nano AndroidProducts.mk
+                nano ../zeroltexx/AndroidProducts.mk
+                nano ../zeroltecan/AndroidProducts.mk
+                nano ../zerofltecan/AndroidProducts.mk
+                nano vendorsetup.sh
+                nano ../zeroltexx/vendorsetup.sh
+                nano ../zeroltecan/vendorsetup.sh
+                nano ../zerofltecan/vendorsetup.sh
+done
