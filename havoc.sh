7bf9d62fa6af7f9a8e5bb03bb6136bda829ce2df
SOBP <42330344+SOBP@users.noreply.github.com>
2019-04-16 22:14:22 +0900

    Update havoc.sh

--- havoc.sh
+++ havoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-                cd device/samsung/zerofltexx && nano BoardConfig.mk && nano havoc_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/havoc_zeroltexx.mk && nano ../zeroltexx/havoc_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/havoc_zeroltecan.mk && nano ../zeroltecan/havoc_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/havoc_zerofltecan.mk && nano ../zerofltecan.mk/havoc_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
+                cd device/samsung/zerofltexx && nano BoardConfig.mk && cp lineage_zerofltexx.mk havoc_zerofltexx.mk && nano havoc_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/havoc_zeroltexx.mk && nano ../zeroltexx/havoc_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/havoc_zeroltecan.mk && nano ../zeroltecan/havoc_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/havoc_zerofltecan.mk && nano ../zerofltecan.mk/havoc_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
                 nano AndroidProducts.mk
                 nano ../zeroltexx/AndroidProducts.mk
                 nano ../zeroltecan/AndroidProducts.mk
@@ -8,4 +8,3 @@
                 nano ../zeroltexx/vendorsetup.sh
                 nano ../zeroltecan/vendorsetup.sh
                 nano ../zerofltecan/vendorsetup.sh
-done
