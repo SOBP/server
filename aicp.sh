ff3375d5e0343a92ec3bc89ed4e85a7683de3f69
SOBP <42330344+SOBP@users.noreply.github.com>
2019-04-16 22:14:56 +0900

    Update aicp.sh

--- aicp.sh
+++ aicp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-                cd device/samsung/zerofltexx && nano BoardConfig.mk && nano aicp_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/aicp_zeroltexx.mk && nano ../zeroltexx/aicp_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/aicp_zeroltecan.mk && nano ../zeroltecan/aicp_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/aicp_zerofltecan.mk && nano ../zerofltecan.mk/aicp_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
+                cd device/samsung/zerofltexx && nano BoardConfig.mk && cp lineage_zerofltexx.mk aicp_zerofltexx.mk && nano aicp_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/aicp_zeroltexx.mk && nano ../zeroltexx/aicp_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/aicp_zeroltecan.mk && nano ../zeroltecan/aicp_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/aicp_zerofltecan.mk && nano ../zerofltecan.mk/aicp_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
                 nano AndroidProducts.mk
                 nano ../zeroltexx/AndroidProducts.mk
                 nano ../zeroltecan/AndroidProducts.mk
