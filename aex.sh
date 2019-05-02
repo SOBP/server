0026fddd8a0b06e609c76045f741588aa043a0d7
SOBP <42330344+SOBP@users.noreply.github.com>
2019-05-02 23:03:41 +0900

    Update aex.sh

--- aex.sh
+++ aex.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
                 cd device/samsung/zerofltexx && nano BoardConfig.mk && nano aosp_zerofltexx.mk && cp ../zeroltexx/lineage_zeroltexx.mk ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/aosp_zeroltexx.mk && nano ../zeroltexx/BoardConfig.mk && cp ../zeroltecan/lineage_zeroltecan.mk ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/aosp_zeroltecan.mk && nano ../zeroltecan/BoardConfig.mk && cp ../zerofltecan/lineage_zerofltecan.mk  ../zerofltecan/aosp_zerofltecan.mk && nano ../zerofltecan/aosp_zerofltecan.mk && nano ../zerofltecan/BoardConfig.mk
-                nano AndroidProducts.mk
                 nano ../zeroltexx/AndroidProducts.mk
                 nano ../zeroltecan/AndroidProducts.mk
                 nano ../zerofltecan/AndroidProducts.mk
-                nano vendorsetup.sh
                 nano ../zeroltexx/vendorsetup.sh
                 nano ../zeroltecan/vendorsetup.sh
                 nano ../zerofltecan/vendorsetup.sh
